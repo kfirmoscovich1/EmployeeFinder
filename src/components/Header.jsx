ff6fc76c251ebfdec6040386e5b2e509fabea9d0
Kfir Moscovich <kfirmoscovich1@gmail.com>
2025-05-27 16:35:21 +0300

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BsPeople, BsStar, BsInfoCircle } from 'react-icons/bs';
+import { FaReact } from "react-icons/fa6";
 
 export default function Header() {
   return (
     <header>
       <div className="header d-flex justify-center align-items-center">
+        <div className="logo d-flex align-items-center">
+          <FaReact className="logo-icon" />
+        </div>
         <nav>
           <Link to="/"><BsPeople /> Employees</Link>
           <Link to="/favorites"><BsStar /> Favorites</Link>
